12d86eeb47bb0bdf796acc5087142a4bbe2d59e8
Drew <drewedgette@gmail.com>
2022-08-12 14:47:16 -0400

    api call happens after button press

--- src/GPT.js
+++ src/GPT.js
@@ -7,43 +7,39 @@ export class GPT extends Component {
       super(props)
     
       this.state = {
-         response: "awaiting response..."
+         response: ""
         }
     }
 
-    handleButton() {
+    handleButton = async () => {
+      this.setState({response: "awaiting response..."});
+
       const configuration = new Configuration({
         apiKey: process.env.REACT_APP_GPT_API_KEY,
         });
-
-        const openai = new OpenAIApi(configuration);
         
-        const response = openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: "list the first ten primes:",
-        temperature: 0,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        })
-        .then( (response) => {
-            this.setState({
-                response: `${response.data.choices[0].text}`
-            })
-        });
+      const openai = new OpenAIApi(configuration);
+      
+      const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: "list the first ten primes:",
+      temperature: 0,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      });
+      
+      this.setState({response: `${response.data.choices[0].text}`});
     }
 
-    componentDidMount() {
-    
-    }
 
   render() {
     const { response } = this.state
     return (
       <div>
         response: {response}
-        <button type="button">Click</button>
+        <button onClick={this.handleButton}>Click</button>
       </div>
     )
   }
